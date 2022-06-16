25ff643eaad264628cb238fc8be3003198156f53
zoujian <zoujian@leju.com>
2022-06-16 17:31:05 +0800

    提交

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"//导入mysql驱动
@@ -15,19 +14,29 @@ type Todo struct {
 	IgnoreMe int `gorm:"-"` // 忽略本字段
 }
 
+//初始化
+var (
+	DB *gorm.DB
+)
+
+//连接数据库
+func initMysql() (err error){
+	DB, err = gorm.Open("mysql","root:@(localhost:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
+	return err
+	//return DB.DB().Ping() //ping不通的话 sql: database is closed
+}
 func main()  {
-	//创建数据库 本地 test库
+	//创建数据库 本地test库
 	//连接数据库
-	db, err := gorm.Open("mysql","root:@(localhost:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
+	err := initMysql()
 	if err != nil {
-		fmt.Println("错误了")
 		panic(err)
 	}
-	//延时关闭
-	defer db.Close()
+	//延时关闭数据库连接
+	defer DB.Close()
 	//表结构迁移
-	db.AutoMigrate(&Todo{})
-
+	DB.AutoMigrate(&Todo{})
+	//实例化引擎
 	r := gin.Default()
 
 	r.Static("/static","./static")// /static/css/app.8eeeaf31.css等资源文件 从 ./static下找 告诉gin框架模板文件引用的静态文件去哪里找
@@ -43,7 +52,7 @@ func main()  {
 		//查询列表
 		v1Group.GET("/todo", func(c *gin.Context) {
 			var todos []Todo
-			db.Debug().Find(&todos)
+			DB.Debug().Find(&todos)
 			c.JSON(http.StatusOK,gin.H{
 				"msg" : "list",
 				"TodoList" : todos,
@@ -61,7 +70,7 @@ func main()  {
 			_ = c.BindJSON(&todo) //绑定参数
 			//.ShouldBind()强大的功能，它能够基于请求自动提取JSON、form表单和QueryString类型的数据，并把值绑定到指定的结构体对象
 			//_ = c.ShouldBind(&todo)
-			db.Debug().Create(&todo)
+			DB.Debug().Create(&todo)
 			//todo 如何判断 是否数据库里已经有？？？
 			c.JSON(http.StatusOK,gin.H{
 				"msg" : "success",
