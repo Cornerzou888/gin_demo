79506e2a59db9c775c18dfdae374f284b22bc035
zoujian <zoujian@leju.com>
2022-06-16 14:35:32 +0800

    提交

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"net/http"
 )
+//代办 model
+type Todo struct {
+	ID uint `json:"id"`
+	Title string `json:"title" gorm:"unique_index;not null"`
+	Status bool `json:"status"`
+	IgnoreMe int `gorm:"-"` // 忽略本字段
+}
 
 func main()  {
+	//创建数据库 本地 test库
+	//连接数据库
+	db, err := gorm.Open("mysql","root:@(localhost:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println("错误了")
+		panic(err)
+	}
+	//延时关闭
+	defer db.Close()
+	//表结构迁移
+	db.AutoMigrate(&Todo{})
 
 	r := gin.Default()
 
@@ -19,10 +40,16 @@ func main()  {
 	//定义路由组
 	v1Group := r.Group("/v1")
 	{
-		//查询
+		//查询列表
 		v1Group.GET("/todo", func(c *gin.Context) {
 			c.JSON(http.StatusOK,gin.H{
 				"msg" : "list",
+			})
+		})
+		//查询某一个
+		v1Group.GET("/todo/:id", func(c *gin.Context) {
+			c.JSON(http.StatusOK,gin.H{
+				"msg" : "info",
 			})
 		})
 		//创建
@@ -46,7 +73,6 @@ func main()  {
 		})
 
 	}
-
 
 	r.Run(":9090")
 
