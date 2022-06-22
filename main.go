591611bdfe554dc6981c4d74252387e7a4c575fb
zoujian <zoujian@leju.com>
2022-06-22 11:21:49 +0800

    提交

--- main.go
+++ main.go
@@ -20,15 +20,19 @@ var (
 )
 
 //连接数据库
-func initMysql() (err error){
-	DB, err = gorm.Open("mysql","root:@(localhost:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
+func initMysql(env string) (err error){
+	if env == "release" {
+		DB, err = gorm.Open("mysql","root:12345@(localhost:13306)/test?charset=utf8mb4&parseTime=True&loc=Local")
+	}else{
+		DB, err = gorm.Open("mysql","root:@(localhost:3306)/test?charset=utf8mb4&parseTime=True&loc=Local")
+	}
 	return err
 	//return DB.DB().Ping() //ping不通的话 sql: database is closed
 }
 func main()  {
 	//创建数据库 本地test库
 	//连接数据库
-	err := initMysql()
+	err := initMysql("release")
 	if err != nil {
 		panic(err)
 	}
